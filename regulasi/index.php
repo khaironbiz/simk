98eec6a434ee7eb2b142843ad621ade3d36ff9b8
khaironbiz <khaironbiz@gmail.com>
2021-10-13 11:41:39 +0700

    table theme

--- regulasi/index.php
+++ regulasi/index.php
@@ -1,7 +1,7 @@
 <?php
 include("../auth/session.php");
 $judul      = "Jenis Regulasi";
-$template   = "../theme/table.php";
+$template   = "../theme/table-simple.php";
 $wrapp      = "../core/wrapp.php";
 $content    = "../views/regulasi/index.php";
 include($template);
---
+++ theme/table-simple.php
@@ -0,0 +1,34 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title><?= $judul; ?></title>
+
+  <!-- Google Font: Source Sans Pro -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="<?= $site_url;?>/assets/AdminLTE/plugins/fontawesome-free/css/all.min.css">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="<?= $site_url;?>/assets/AdminLTE/dist/css/adminlte.min.css">
+</head>
+<body class="hold-transition sidebar-mini">
+
+<?php
+include($wrapp);
+
+?>
+
+<!-- ./wrapper -->
+
+<!-- jQuery -->
+<script src="<?= $site_url;?>/assets/AdminLTE/plugins/jquery/jquery.min.js"></script>
+<!-- Bootstrap 4 -->
+<script src="<?= $site_url;?>/assets/AdminLTE/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- AdminLTE App -->
+<script src="<?= $site_url;?>/assets/AdminLTE/dist/js/adminlte.min.js"></script>
+<!-- AdminLTE for demo purposes -->
+<script src="<?= $site_url;?>/assets/AdminLTE/dist/js/demo.js"></script>
+</body>
+</html>
