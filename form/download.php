d2508accaa9a9cb7dcc80bf839bc613048aa3dc5
khaironbiz <khaironbiz@gmail.com>
2021-10-15 15:45:31 +0700

    mac

--- form/download.php
+++ form/download.php
@@ -1,5 +1,19 @@
 <?php
 include("../auth/session.php");
+// PHP code to get the MAC address of Client
+$MAC = exec('getmac');
+  
+// Storing 'getmac' value in $MAC
+$MAC = strtok($MAC, ' ');
+  
+// Updating $MAC value using strtok function, 
+// strtok is used to split the string into tokens
+// split character of strtok is defined as a space
+// because getmac returns transport name after
+// MAC address   
+
+
+
 $time               = date('Y-m-d H:i:s');
 $key                = $_GET['id'];
 $sql_key            = mysqli_query($host,"SELECT * FROM form_detail WHERE has_form_detail='$key'");
@@ -7,6 +21,7 @@ $data               = mysqli_fetch_array($sql_key);
 $id_form_detail     = $data['id_form_detail'];
 $id_form            = $data['id_form'];
 $ip                 = $_SERVER['REMOTE_ADDR'];
+$perangkat          = $_SERVER['HTTP_USER_AGENT'];
 $url                = $site_url."/assets/files/form/".$data['form_file'];
 $hit_num            = $data['count_hit']+1;
 $has_form_downloader= md5(uniqid());
@@ -20,6 +35,7 @@ $downloader         = mysqli_query($host,"INSERT INTO form_downloader SET
                                 id_perawat          = '$user_check',
                                 waktu_download      = '$time',
                                 ip                  = '$ip',
+                                perangkat           = '$MAC',
                                 has_form_downloader = '$has_form_downloader'");
 if($update_data){
 header("location:$url");
