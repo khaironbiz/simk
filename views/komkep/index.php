8ba6d130eba7a4f815259fc827188afe87cad3e5
khaironbiz <khaironbiz@gmail.com>
2021-11-20 19:25:54 +0700

    usia

--- views/komkep/index.php
+++ views/komkep/index.php
@@ -75,44 +75,71 @@
                     </div>
                   </form>
                   <?php
-                  include('aksi/jenis-regulasi.php');
+                  //include('aksi/jenis-regulasi.php');
                   ?>
                   <table id="example1" class="table table-bordered table-striped">
                     <thead>
                       <tr>
                         <th>#</th>
-                        <th>Jenis Regulasi</th>
-                        <th>Jumlah Regulasi</th>
+                        <th>Rentang Usia</th>
+                        <th>Count </th>
                         <th>Aksi</th>
                       </tr>
                     </thead>
                     <tbody>
                       <?php
-                      $no                   = 1;
-                      $sql_perawat          = mysqli_query($host, "SELECT * FROM regulasi_jenis ORDER BY jenis_regulasi");
-                      while($data           = mysqli_fetch_array($sql_perawat)){
-                        $id_regulasi_jenis  = $data['id_regulasi_jenis'];
-                        $sql_count          = mysqli_query($host, "SELECT * FROM regulasi_detail WHERE id_regulasi_jenis='$id_regulasi_jenis'");
-                        $count_data         = mysqli_num_rows($sql_count);
+                      
+                      $th_20      = date('Y-m-d', strtotime('-20 year', strtotime(date('Y-m-d'))));
+                      $th_25      = date('Y-m-d', strtotime('-25 year', strtotime(date('Y-m-d'))));
+                      $th_30      = date('Y-m-d', strtotime('-30 year', strtotime(date('Y-m-d'))));
+                      $th_35      = date('Y-m-d', strtotime('-35 year', strtotime(date('Y-m-d'))));
+                      $th_40      = date('Y-m-d', strtotime('-40 year', strtotime(date('Y-m-d'))));
+                      $th_50      = date('Y-m-d', strtotime('-50 year', strtotime(date('Y-m-d'))));
+                      $th_60      = date('Y-m-d', strtotime('-60 year', strtotime(date('Y-m-d'))));
+                      $no         = 1;
+                      $sql_20_25  = mysqli_query($host, "SELECT * FROM nira where blokir ='N' and ttl between '$th_25' and '$th_20' ");
+                      $sql_25_30  = mysqli_query($host, "SELECT * FROM nira where blokir ='N' and ttl between '$th_30' and '$th_25' ");
+                      $sql_30_40  = mysqli_query($host, "SELECT * FROM nira where blokir ='N' and ttl between '$th_40' and '$th_30'");
+                      $sql_40_50  = mysqli_query($host, "SELECT * FROM nira where blokir ='N' and ttl between '$th_50' and '$th_40'");
+                      $sql_50_60  = mysqli_query($host, "SELECT * FROM nira where blokir ='N' and ttl between '$th_60' and '$th_50'  ");
+                      $sql_30_35  = mysqli_query($host, "SELECT * FROM nira where blokir ='N' and ttl between '$th_35' and  '$th_30'");
+                      //$count_20_30  = mysqli_num_rows($sql_20_30);
                       ?>
                       <tr>
-                        <td width="10px"><?= $no++; ?></td>
-                        <td><?= $data['jenis_regulasi'];?></td>
-                        <td><?= $count_data;?></td>
-                        <td><a href="<?= $site_url ?>/regulasi/detail.php?id=<?= $data['has_regulasi_jenis']?>" class="btn btn-primary btn-sm">Detail</a></td>
+                        <td width="10px">1</td>
+                        <td> Usia 20-25 Tahun</td>
+                        <td><?= mysqli_num_rows($sql_20_25);?></td>
+                        <td><a href="detail.php?has=<?= $th_25 ?>&key=<?= $th_20 ?>" class="btn btn-success btn-sm"><?= $th_25 ?></a></td>
+                      </tr>
+                      <tr>
+                        <td width="10px">1</td>
+                        <td> Usia 25-30 Tahun</td>
+                        <td><?= mysqli_num_rows($sql_25_30);?></td>
+                        <td><a href="detail.php?has=<?= $th_30 ?>&key=<?= $th_25 ?>" class="btn btn-success btn-sm"><?= $th_30 ?></a></td>
+                      </tr>
+                      <tr>
+                        <td width="10px">2</td>
+                        <td> Usia 30-40 Tahun</td>
+                        <td><?= mysqli_num_rows($sql_30_40)?></td>
+                        <td><a href="detail.php?has=<?= $th_40 ?>&key=<?= $th_30 ?>" class="btn btn-success btn-sm"><?= $th_40 ?></a></td>
+                      </tr>
+                      <tr>
+                        <td width="10px">3</td>
+                        <td> Usia 40-50 Tahun</td>
+                        <td><?= mysqli_num_rows($sql_40_50)?></td>
+                        <td><a href="detail.php?has=<?= $th_50 ?>&key=<?= $th_40 ?>" class="btn btn-success btn-sm"><?= $th_50 ?></a></td>
+                      </tr>
+                      <tr>
+                        <td width="10px">4</td>
+                        <td> Usia 50-60 Tahun</td>
+                        <td><?= mysqli_num_rows($sql_50_60)?></td>
+                        <td><a href="detail.php?has=<?= $th_60 ?>&key=<?= $th_50 ?>" class="btn btn-success btn-sm"><?= $th_60 ?></a></td>
                       </tr>
                       <?php
-                        }
+                        // }
                       ?>
                     </tbody>
-                    <tfoot>
-                      <tr>
-                        <th>#</th>
-                        <th>Jenis Regulasi</th>
-                        <th>Jumlah Regulasi</th>
-                        <th>Aksi</th>
-                      </tr>
-                    </tfoot>
+                    
                   </table>
                 </div>
                 <!-- /.card-body -->
