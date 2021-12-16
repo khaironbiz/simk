f6e928cb31158d88b02c190b5a1ac64ebc3e42e0
khaironbiz <khaironbiz@gmail.com>
2021-12-16 12:43:07 +0700

    add url

--- auth/site.php
+++ auth/site.php
@@ -1,7 +1,12 @@
 <?php
-//site local
-//$site_url           = "http://localhost/simk";
-$site_url           = "https://ppni.rspon.net/simk";
+//site local atau hosting
+$server=2;
+if($server==1){
+$site_url           = "http://localhost/simk";
+}elseif($server==2){
+    $site_url           = "https://ppni.rspon.net/simk";
+}
+
 $nama_web           = "DPK PPNI RSPON";
 $nama_perusahaan    = "DPK PPNI RSPON";
 $alamat_perusahaan  = "Jl. MT Haryono Kavling 11 Cawang, Jakarta Timur, DKI Jakarta 13630";
@@ -9,6 +14,5 @@ $deskripsi_web      = "Sistem Informasi Manajemen Anggota";
 $version_web        = "2.0.0";
 ?>
 <script>
-    
-    var base_url = $site_url;
+    var base_url = <?= $site_url ?>;
 </script>
--- views/pasien/menu/index.php
+++ views/pasien/menu/index.php
@@ -6,7 +6,7 @@
                         <div class="collapse navbar-collapse" id="navbarSupportedContent">
                             <ul class="navbar-nav mr-auto">
                                 <li class="nav-item active">
-                                    <a class="nav-link" href="#"><?= $_SERVER['SCRIPT_NAME']?> <span class="sr-only">(current)</span></a>
+                                    <a class="nav-link" href="#"><?= $_SERVER['REQUEST_METHOD']?> <span class="sr-only">(current)</span></a>
                                 </li>
                                 <li class="nav-item">
                                     <a class="nav-link" href="#">Kunjungan</a>
