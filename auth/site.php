9ded06208a2a9c732f791ef48b9d6f192a68094d
khaironbiz <khaironbiz@gmail.com>
2021-12-16 12:22:56 +0700

    revisi url

--- auth/site.php
+++ auth/site.php
@@ -1,7 +1,7 @@
 <?php
 //site local
-$site_url           = "http://".$_SERVER['SERVER_NAME']."/simk";
-//$site_url           = "https://ppni.rspon.net/simk";
+//$site_url           = "http://localhost/simk";
+$site_url           = "https://ppni.rspon.net/simk";
 $nama_web           = "DPK PPNI RSPON";
 $nama_perusahaan    = "DPK PPNI RSPON";
 $alamat_perusahaan  = "Jl. MT Haryono Kavling 11 Cawang, Jakarta Timur, DKI Jakarta 13630";
@@ -9,6 +9,6 @@ $deskripsi_web      = "Sistem Informasi Manajemen Anggota";
 $version_web        = "2.0.0";
 ?>
 <script>
+    
     var base_url = $site_url;
-    //var base_url = "https://ppni.rspon.net/simk";
 </script>
