15d0808b0da7180cb79944bfb3ee8b15f1b3760d
khaironbiz <khaironbiz@gmail.com>
2022-01-14 19:38:21 +0700

    edit page not found

--- pasien/detail.php
+++ pasien/detail.php
@@ -2,11 +2,20 @@
 include("../auth/session.php");
 $key        = $_GET['key'];
 $sql_pasien = mysqli_query($host,"SELECT * FROM pasien_db WHERE has_pasien_db='$key'");
+$count_pasien=mysqli_num_rows($sql_pasien);
 $data_pasien= mysqli_fetch_array($sql_pasien);
-$judul      = $data_pasien['nama_pasien'];
-$template   = "../theme/table.php";
-$wrapp      = "../core/wrapp.php";
-$content    = "../views/pasien/detail.php";
+if($count_pasien < 1){
+    $judul      = "Page Not Found";
+    $template   = "../theme/table.php";
+    $wrapp      = "../core/wrapp.php";
+    $content    = "../views/page/not-found.php";
+}else{
+    $judul      = $data_pasien['nama_pasien'];
+    $template   = "../theme/table.php";
+    $wrapp      = "../core/wrapp.php";
+    $content    = "../views/pasien/detail.php";
+}
+
 include($template);
 
 
