be9656cd5865a3e1860ceedd80693085238aa066
khaironbiz <khaironbiz@gmail.com>
2022-01-24 16:26:32 +0700

    php mailer

---
+++ mail/send_email.php
@@ -0,0 +1,46 @@
+<?php
+$email_penerima     = "khaironbiz@gmail.com";
+$nama_penerima      = "HPII";
+$email_pengirim     = "hpii.ppni@gmail.com";
+$nama_pengirim      = "HPII";
+//Import PHPMailer classes into the global namespace
+//These must be at the top of your script, not inside a function
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+//Load Composer's autoloader
+require 'vendor/autoload.php';
+//Create an instance; passing `true` enables exceptions
+$mail = new PHPMailer(true);
+try {
+    //Server settings
+    //$mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+    $mail->isSMTP();                                            //Send using SMTP
+    $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+    $mail->Username   = 'hpii.ppni@gmail.com';                     //SMTP username
+    $mail->Password   = '@Mail250909#';                               //SMTP password
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
+    $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+    //Recipients
+    $mail->setFrom($email_pengirim, $nama_pengirim);
+    $mail->addAddress($email_penerima, $nama_penerima);     //Add a recipient
+    //$mail->addAddress('ellen@example.com');               //Name is optional
+    $mail->addReplyTo($email_pengirim, $nama_pengirim);
+    // $mail->addCC('cc@example.com');
+    // $mail->addBCC('bcc@example.com');
+    //Attachments
+    $mail->addAttachment('assets/file/SertifikatHIPENI.pdf');         //Add attachments
+    // $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
+    //Content
+    $mail->isHTML(true);                                  //Set email format to HTML
+    $mail->Subject = 'Here is the subject';
+    $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+    $mail->send();
+    //echo 'Message has been sent';
+} 
+catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: ";
+}
+?>
