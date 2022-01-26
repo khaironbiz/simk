3960f4186cce4dd4c7876e68476dc0d3220ed996
khaironbiz <khaironbiz@gmail.com>
2022-01-26 16:06:09 +0700

    koneksi

--- auth/koneksi.php
+++ auth/koneksi.php
@@ -1,7 +1,7 @@
 <?php 
 //koneksi ke local
-//$host = mysqli_connect("153.92.7.151", "u487816097_ppni", "domain250909", "u487816097_data");
-$host = mysqli_connect("103.16.133.234", "phpmyadmin", "inifgrup", "khairon_ppni");
+$host = mysqli_connect("153.92.7.151", "u487816097_ppni", "domain250909", "u487816097_data");
+//$host = mysqli_connect("103.16.133.234", "phpmyadmin", "inifgrup", "khairon_ppni");
 //koneksi ke hosting
 //$host = mysqli_connect("localhost", "u853814933_uas", "Sql250909", "u853814933_uas");
 
