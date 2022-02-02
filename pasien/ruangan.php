4d7fe2a4325becdecd89d4236b25a9fc158b64a8
khaironbiz <khaironbiz@gmail.com>
2022-02-02 10:26:26 +0700

    function

--- pasien/ruangan.php
+++ pasien/ruangan.php
@@ -1,6 +1,15 @@
 <?php
 include("../auth/session.php");
 //include("../function/function.php");
+include("../function/diagnosa.php");
+include("../function/master.php");
+include("../function/newss.php");
+include("../function/pasien.php");
+include("../function/perawat.php");
+include("../function/ruangan.php");
+include("../function/shift.php");
+include("../function/usia.php");
+include("../function/wilayah.php");
 $judul          = "Pasien Dirawat";
 $template       = "../theme/table-simple.php";
 $wrapp          = "../core/wrapp.php";
