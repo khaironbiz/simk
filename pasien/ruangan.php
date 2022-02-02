8015482e4129d2458d930d00db11d44f55a4ae9b
khaironbiz <khaironbiz@gmail.com>
2022-02-02 10:23:14 +0700

    pasien

--- pasien/ruangan.php
+++ pasien/ruangan.php
@@ -1,7 +1,7 @@
 <?php
 include("../auth/session.php");
-include("../function/function.php");
-$judul          = "Bed Manajemen";
+//include("../function/function.php");
+$judul          = "Pasien Dirawat";
 $template       = "../theme/table-simple.php";
 $wrapp          = "../core/wrapp.php";
 $content        = "../views/pasien/pasien-ruangan.php";
