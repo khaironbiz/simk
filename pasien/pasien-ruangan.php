453df824ee0d432891f75bab993bb5bd7ea63071
khaironbiz <khaironbiz@gmail.com>
2022-02-02 10:14:04 +0700

    pasien ruangan

--- pasien/pasien-ruangan.php
+++ pasien/pasien-ruangan.php
@@ -4,7 +4,7 @@ include("../function/function.php");
 $judul          = "Bed Manajemen";
 $template       = "../theme/table-simple.php";
 $wrapp          = "../core/wrapp.php";
-$content        = "../views/pasien/pasien-ruangan.php";
+$content        = "../views/pasien/ruangan.php";
 include($template);
 
 
---
+++ views/pasien/ruangan.php
@@ -0,0 +1,1 @@
+<?= "khairon Here"?>
