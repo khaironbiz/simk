ed0d23e71f52e4757d3e505396a21ad59ab92b22
khaironbiz <khaironbiz@gmail.com>
2023-04-11 12:00:47 +0700

    notif

--- dokumen-legal/sipp-bermasalah.php
+++ dokumen-legal/sipp-bermasalah.php
@@ -25,6 +25,7 @@
                             <th>NIRA</th>
                             <th>SIPP</th>
                             <th>Masa Berlaku</th>
+                            <th>Notif</th>
                         </tr>
                         <?php
                         include('../auth/koneksi.php');
@@ -39,9 +40,115 @@
                             <td><?= $data['nira']?></td>
                             <td><?= $data['sipp']?></td>
                             <td><?= $data['sipp_ahir']?></td>
+                            <td>
+                                <!-- Button trigger modal -->
+                                <button type="button" class="btn btn-sm btn-danger" data-bs-toggle="modal" data-bs-target="#exampleModal<?= $data['nira']?>">
+                                    Notif
+                                </button>
+
+                                <!-- Modal -->
+                                <div class="modal fade" id="exampleModal<?= $data['nira']?>" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                    <div class="modal-dialog">
+                                        <div class="modal-content">
+                                            <form action="" method="post">
+                                                <div class="modal-header">
+                                                    <h5 class="modal-title" id="exampleModalLabel">Pemberitahuan SIPP </h5>
+                                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                                </div>
+                                                <div class="modal-body">
+                                                    <table class="table table-sm">
+                                                        <tr>
+                                                            <td>Nama</td>
+                                                            <td>:</td>
+                                                            <td>
+                                                                <input type="text" name="nama" value="<?= $data['nama']?>" readonly class="form-control form-control-sm" >
+                                                            </td>
+                                                        </tr>
+                                                        <tr>
+                                                            <td>NIRA</td>
+                                                            <td>:</td>
+                                                            <td>
+                                                                <input type="text" name="nira" value="<?= $data['nira']?>" readonly class="form-control form-control-sm" >
+                                                            </td>
+                                                        </tr>
+                                                        <tr>
+                                                            <td>SIPP</td>
+                                                            <td>:</td>
+                                                            <td>
+                                                                <input type="text" name="sipp" value="<?= $data['sipp']?>" readonly class="form-control form-control-sm" >
+                                                            </td>
+                                                        </tr>
+
+                                                        <tr>
+                                                            <td>SIPP Exp</td>
+                                                            <td>:</td>
+                                                            <td>
+                                                                <input type="date" name="sipp_ahir" value="<?= $data['sipp_ahir']?>" readonly class="form-control form-control-sm" >
+                                                            </td>
+                                                        </tr>
+                                                        <tr>
+                                                            <td>STR</td>
+                                                            <td>:</td>
+                                                            <td>
+                                                                <input type="text" name="str" value="<?= $data['str']?>" readonly class="form-control form-control-sm" >
+                                                            </td>
+                                                        </tr>
+
+                                                        <tr>
+                                                            <td>STR Exp</td>
+                                                            <td>:</td>
+                                                            <td>
+                                                                <input type="date" name="str_ahir" value="<?= $data['str_ahir']?>" readonly class="form-control form-control-sm" >
+                                                            </td>
+                                                        </tr>
+                                                        <tr>
+                                                            <td>HP</td>
+                                                            <td>:</td>
+                                                            <td>
+                                                                <input type="text" name="hp" value="<?= $data['hp']?>" readonly class="form-control form-control-sm" >
+                                                            </td>
+                                                        </tr>
+                                                    </table>
+                                                </div>
+                                                <div class="modal-footer">
+                                                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                                    <button type="submit" class="btn btn-danger">Kirim Pesan</button>
+                                                </div>
+
+                                            </form>
+
+                                        </div>
+                                    </div>
+                                </div>
+                            </td>
                         </tr>
                         
                         <?php
+                        }
+                        if(isset($_POST['str_ahir'])){
+                            $nama       = $_POST['nama'];
+                            $nira       = $_POST['nira'];
+                            $str        = $_POST['str'];
+                            $str_ahir   = $_POST['str_ahir'];
+                            $sipp       = $_POST['sipp'];
+                            $sipp_ahir  = $_POST['sipp_ahir'];
+                            $hp         = $_POST['hp'];
+                            $time       = date('Y-m-d H:i:s');
+                            $insert     = mysqli_query($host,"INSERT INTO notifikasi SET 
+                                            pengirim    = 'pengirim',
+                                            penerima    = '$nira',
+                                            time_notif  = '$time'");
+                            if($insert ){
+                                $text = "
+                                nama : $nama,
+                                nira : $nira,
+                                str : $str $str_ahir,
+                                sipp : $sipp $sipp_ahir
+                                ";
+
+                                echo "<script>window.open('https://api.whatsapp.com/send?phone=62$hp&text=hallo nama: $nama, nira: $nira, str : $str exp : $str_ahir, sipp : $sipp exp : $sipp_ahir, segera proses dokumen legal saudara dan update dokumen legal pada simk ppni rspon, terimakasih ---- ini adalah notif dari simk ppni rspon -----','_blank')</script>";
+                                echo "<script>document.location=\"sipp-bermasalah.php\"</script>";
+                            }
                         }
                         ?>
                     </table>
